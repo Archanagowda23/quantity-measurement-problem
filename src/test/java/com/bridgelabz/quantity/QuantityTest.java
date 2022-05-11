29be6d15736303158980159ff6bd2b07f86144c8
Archanagowda23 <archanar249@gmail.com>
2022-05-11 07:37:36 +0530

    Added: Wish To Compare Lengths ft & Inches

--- src/test/java/com/bridgelabz/quantity/QuantityTest.java
+++ src/test/java/com/bridgelabz/quantity/QuantityTest.java
@@ -1,4 +1,15 @@
 package com.bridgelabz.quantity;
 
+import org.junit.Test;
+import org.testng.Assert;
+
+//Test Case 1.1
 public class QuantityTest {
+    @Test
+    public void given0Feetand0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
 }
